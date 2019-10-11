be158b39de1c76c4808dca76d76a1813f85b5ef9
André Sebastian <a.sebastian@posteo.de>
2019-10-11 21:14:58 +0200

    make own first scroll possible

---
+++ react-plx/src/components/Paralax.jsx
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import Plx from 'react-plx';
+
+// An array of parallax effects to be applied - see below for detail
+const parallaxData = [
+  {
+    start: 0,
+    end: 500,
+    properties: [
+      {
+        startValue: 1,
+        endValue: 4.5,
+        property: 'scale',
+      },
+    ],
+  },
+];
+
+const exampleParallaxData = [
+    {
+      start: 0,
+      end: 300,
+      properties: [
+        {
+          startValue: 0,
+          endValue: 180,
+          property: "rotate"
+        },
+        {
+          startValue: 1,
+          endValue: 1.5,
+          property: "scale"
+        },
+        {
+          startValue: 1,
+          endValue: 0.0,
+          property: "opacity"
+        }
+      ]
+    },
+    {
+      start: 350,
+      duration: 300,
+      properties: [
+        {
+          startValue: "#3cb99c",
+          endValue: "rgba(250,50,200,0.8)",
+          property: "backgroundColor"
+        },
+        {
+          startValue: 30,
+          endValue: 100,
+          property: "translateY"
+        },
+        {
+          startValue: 0.75,
+          endValue: 1,
+          property: "opacity"
+        }
+      ]
+    },
+    {
+      start: 700,
+      duration: 1000,
+      properties: [
+        {
+          startValue: 100,
+          endValue: 0,
+          property: "translateY"
+        },
+        {
+          startValue: 1.5,
+          endValue: 0.4,
+          property: "scale"
+        },
+        {
+          startValue: 90,
+          endValue: 0,
+          property: "rotate"
+        },
+        // Blur is not performant
+        // Used just as an example for CSS filters
+        {
+          startValue: 0,
+          endValue: 2,
+          property: "blur"
+        }
+      ]
+    }
+  ];
+
+let style = {
+    left: 50,
+    top: 100,
+    width: 300,
+    backgroundColor: "#34ba9c",
+    height: 300,
+    borderRadius: 20,
+    lineHeight: "100px",
+    textAlign: "center",
+    position: "fixed",
+}
+
+const styles = {
+    width: 200,
+    height: 100,
+    lineHeight: "100px",
+    textAlign: "center",
+    borderRadius: 20,
+    backgroundColor: "#34ba9c",
+    color: "#fff",
+    left: "50%",
+    marginLeft: -50,
+    top: 100,
+    position: "fixed",
+    fontFamily: "Helvetica, Arial, sans-serif"
+  };
+  
+
+export default class Example extends Component {
+  render() {
+    return (
+        <div style={{ height: 3000 }}>
+    <Plx
+        className='MyAwesomeParallax'
+        parallaxData={ parallaxData }
+        style = {styles}
+      >
+          test
+      </Plx>
+        </div>
+  
+    );
+  }
+}
